ca02ee092efb8bf8ac029c4610df0049b2c80bf5
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2021-09-05 07:38:00 +0300

    Add project files.

---
+++ children-of-devin-back-end/Data/Models/Story.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace children_of_devin_back_end.Data.Models
+{
+    public class Story
+    {
+        public TYPE Type { get; set; }
+    }
+}
